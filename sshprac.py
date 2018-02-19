e747c2bce48f22240f41ae2d402b26e9283e7567
Solomon DellaPenna <solomondellapenna@gmail.com>
2018-02-19 13:23:06 -0600

    yuh

--- sshprac.py
+++ sshprac.py
@@ -8,13 +8,16 @@ key = paramiko.RSAKey.from_private_key_file("/Users/solomondellapenna/py/abc.pem
 cmd = 'nc -zv 169.254.169.254 80'
 
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(ip,port,username,pkey=key)
-
-stdin,stdout,stderr = ssh.exec_command(cmd)
-outlines = stdout.readlines()
-resp = ''.join(outlines)
-print(resp)
-if 'succeeded' in resp:
-    ssh.exec_command('echo $PATH')
+def sshthing():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip,port,username,pkey=key)
+    stdin,stdout,stderr = ssh.exec_command(cmd)
+    outlines = stdout.readlines()
+    resp = ''.join(outlines)
+    return resp
+    stdin,stdout,stder = ssh.exec_command('echo $PATH')
+    x = stdout.readlines()
+    y = ''.join(x)
+    return y
+print(sshthing())
