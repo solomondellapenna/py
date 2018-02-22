896b87477321a72de6a8cc831136d29b4c0056e0
Solomon DellaPenna <solomondellapenna@gmail.com>
2018-02-22 12:35:25 -0600

    FINAL PRODUCT

--- sshprac.py
+++ sshprac.py
@@ -1,23 +1,26 @@
 import paramiko
-import sys
 
-ip = 'ec2-52-32-95-162.us-west-2.compute.amazonaws.com'
+ip = input('What is your ec2 instance address? ')
 port = 22
 username = 'ec2-user'
-key = paramiko.RSAKey.from_private_key_file("/Users/solomondellapenna/py/abc.pem")
-cmd = 'nc -zv 169.254.169.254 80'
-
+key = paramiko.RSAKey.from_private_key_file(input("What is the path to your private key? "))
+check = 'nc -zv 169.254.169.254 80'
+response = 'sudo iptables -I OUTPUT -d 169.254.169.254 -p tcp --dport 80 -j REJECT'
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(ip,port,username,pkey=key)
 
 def sshthing():
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(ip,port,username,pkey=key)
-    stdin,stdout,stderr = ssh.exec_command(cmd)
+    stdin,stdout,stderr = ssh.exec_command(check)
     outlines = stdout.readlines()
     resp = ''.join(outlines)
     return resp
-    stdin,stdout,stder = ssh.exec_command('echo $PATH')
-    x = stdout.readlines()
-    y = ''.join(x)
-    return y
-print(sshthing())
+def sshthing2():
+    stdin,stdout,stder = ssh.exec_command(response)
+    outlines = stdout.readlines()
+    resp = ''.join(outlines)
+    return resp
+if 'succeeded' in sshthing():
+    sshthing2()
+else:
+    print("Metadata Secure!")
